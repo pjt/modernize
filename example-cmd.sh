02b775a2169d9862c359d01a6f08d701e328115d
Perry Trolard <trolard@gmail.com>
2009-01-14 16:04:49 -0600

    Add source .clj's to jar; added example invocation in example-cmd.

---
+++ example-cmd.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+java -jar modernize.jar data/bigdict-moby.txt data/th-words.txt \
+   "i j" \
+   "u v" \
+   "v u" \
+   "vv w" \
+   "\\u00F5 on om" \
+   "\u0169 un um" \
+   "\u1EDB en em" \
+   "\u00E3 an am"
